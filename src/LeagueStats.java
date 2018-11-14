821f874260889ebd8f49148ecc1c31487b522bf5
ashishdev007 <ashishdevofficial@outlook.com>
2018-11-13 18:47:44 -0600

    "Added instance variables, constructor and getters"

--- src/LeagueStats.java
+++ src/LeagueStats.java
@@ -1,7 +1,45 @@
 public class LeagueStats {
 
     private class Team{
+        private String name;
+        private int wins;
+        private int losses;
+        private int goalsScored;
+        private int goalsAllowed;
+        private int points;
 
+        public Team(String name){
+            this.name = name;
+            this.wins = 0;
+            this.losses = 0;
+            this.goalsScored = 0;
+            this.goalsAllowed = 0;
+            this.points = 0;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getWins() {
+            return wins;
+        }
+
+        public int getLosses() {
+            return losses;
+        }
+
+        public int getGoalsScored() {
+            return goalsScored;
+        }
+
+        public int getGoalsAllowed() {
+            return goalsAllowed;
+        }
+
+        public int getPoints() {
+            return points;
+        }
     }
 }
 
